1a54835823f097611574a57f6b7fc792ea76f312
Christian Lehmann <mail@christianlehmann.xyz>
2022-02-19 20:20:24 +0100

    refactoring and several buttons for button and sheet

--- index.js
+++ index.js
@@ -1,117 +1,129 @@
 const [buttonsForm] = document.forms;
-const [yearInput, submitButton] = buttonsForm;
-const wrapperEl = document.getElementById("wrapper")
+const [yearInput, createButtonBtn, createSheetBtn] = buttonsForm;
 const DPI = 600;
+
+createButtonBtn.addEventListener("click", async () => {
+    const btnCanvas = await createButtonCanvas( yearInput.value );
+    btnCanvas.toBlob( blob => console.log(blob) || changeDpiBlob( blob, DPI).then( blob => {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        a.href = url;
+        a.download = "button_" + yearInput.value + ".png";
+        a.click();
+        URL.revokeObjectURL(url);
+    } ) );
+    
+});
+
+createSheetBtn.addEventListener("click", async () => {
+    const btnCanvas = await createButtonCanvas( yearInput.value );
+    const sheetCanvas = createSheetCanvas( btnCanvas, 4000, 6000 );
+    
+    sheetCanvas.toBlob( async blob => {
+        const blob2 = await changeDpiBlob(blob, DPI);
+        const url = URL.createObjectURL(blob2);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        a.href = url;
+        a.download = "button_sheet_" + yearInput.value + ".png";
+        a.click();
+        URL.revokeObjectURL(url);
+    });
+});
 
 yearInput.value = (new Date()).getFullYear();
 
-buttonsForm.onsubmit = e => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    var img = new Image();
-    
-    img.onload = function(){
+const createButtonCanvas = year => new Promise( resolve => {
+    const img = new Image();
+    img.onload = function() {
         const canvas = document.createElement("canvas");
-        //wrapperEl.innerHTML = "";
-        //wrapperEl.appendChild(canvas);
-        
         canvas.width = this.width;
         canvas.height = this.height;
-                                // 4517, 6050
 
         const ctx = canvas.getContext("2d");
-        
         ctx.drawImage(img,0,0);
         ctx.font         = '312px CoelnischeCurrentFraktur';
         ctx.fillStyle = 'blue';
         
-        const leftDigits = yearInput.value.substring(0,2);
-        const rightDigits = yearInput.value.substring(2);
+        const leftDigits = year.substring(0,2);
+        const rightDigits = year.substring(2);
 
         const measureLeftDigits = ctx.measureText(leftDigits);
 
         ctx.fillStyle = "white";
         ctx.fillText(leftDigits, this.width / 2 - measureLeftDigits.width, this.height / 2);
-    
+
         ctx.fillStyle = "#F54718";
         ctx.fillText(rightDigits, this.width / 2, this.height / 2);
-        ctx.save();
 
-        document.getElementById("preview-img").src = changeDpiDataUrl(canvas.toDataURL(), DPI);
-
-        const sheetCanvas = document.createElement("canvas");
-        sheetCanvas.width = 4517;
-        sheetCanvas.height = 6050;
-
-        const sheetCtx = sheetCanvas.getContext("2d");
-
-        buttonPositionsByWidth( { width: this.width, height: this.height }, { width: 4517, height: 6050 } ).forEach( ([x,y ]) => sheetCtx.drawImage(canvas, x,y) )
-    
-        sheetCanvas.toBlob( blob => {
-            changeDpiBlob(blob, DPI).then( blob2 => {
-                const url = URL.createObjectURL(blob2);
-                const sheetPreviewEl = document.getElementById("sheet-preview");
-                const onLoad = () => {
-                    URL.revokeObjectURL(url);
-                    sheetPreviewEl.removeEventListener("load", onLoad);
-                }
-                sheetPreviewEl.addEventListener("load", onLoad);
-                document.getElementById("sheet-preview").src = URL.createObjectURL(blob2);
-            } );
-        }  );
+        resolve(canvas);
     };
-    img.src = './plain_button.png';
-};
+    img.src = "./plain_button.png";
+});
 
+const createSheetCanvas = ( buttonCanvas, width, height ) => {
 
-const buttonPositionsByWidth = ( buttonDimension, sheetDimension, options = {} )  => {
+    const buttonPositionsByWidth = ( buttonDimension, sheetDimension, options = {} )  => {
 
-    const isFitting = ( top, left, radius ) => top >= 0 && left >= 0 && sheetDimension.width > left + 2 * radius && sheetDimension.height > top + 2 * radius;
-
-    const positions = [];
-    const { padding = 50 } = options;
-    const radius = (Math.max(buttonDimension.width, buttonDimension.height) / 2)+ padding;
+        const isFitting = ( top, left, radius ) => top >= 0 && left >= 0 && sheetDimension.width > left + 2 * radius && sheetDimension.height > top + 2 * radius;
     
-    let left = padding;
-
-    let currentCol = 0;
-
-    while (left < sheetDimension.width) {
-        let top = currentCol % 2 == 0 ? padding : padding - radius;
-        while (top < sheetDimension.height) {
-            if ( isFitting(top, left, radius)) positions.push( [left, top] );
-            top = top + 2 * radius;
-        }
-        left = parseInt(left + (Math.sqrt(3) * radius));
-        currentCol++;
-    }
-
-    return positions;
-};
-
-
-const buttonPositionsByHeight = ( buttonDimension, sheetDimension, options = {} )  => {
-
-    const isFitting = ( top, left, radius ) => top >= 0 && left >= 0 && sheetDimension.width > left + 2 * radius && sheetDimension.height > top + 2 * radius;
-
-    const positions = [];
-    const { padding = 50 } = options;
-    const radius = (Math.max(buttonDimension.width, buttonDimension.height) / 2)+ padding;
+        const positions = [];
+        const { padding = 50 } = options;
+        const radius = (Math.max(buttonDimension.width, buttonDimension.height) / 2)+ padding;
+        
+        let left = padding;
     
-    let top = padding;
-
-    let currentRow = 0;
-
-    while (top < sheetDimension.height) {
-        let left = currentRow % 2 == 0 ? padding : padding - radius;
+        let currentCol = 0;
+    
         while (left < sheetDimension.width) {
-            if ( isFitting(top, left, radius)) positions.push( [left, top] );
-            left = left + 2 * radius;
+            let top = currentCol % 2 == 0 ? padding : padding - radius;
+            while (top < sheetDimension.height) {
+                if ( isFitting(top, left, radius)) positions.push( [left, top] );
+                top = top + 2 * radius;
+            }
+            left = parseInt(left + (Math.sqrt(3) * radius));
+            currentCol++;
         }
-        top = parseInt(top + (Math.sqrt(3) * radius));
-        currentRow++;
-    }
+    
+        return positions;
+    };
+    
+    
+    const buttonPositionsByHeight = ( buttonDimension, sheetDimension, options = {} )  => {
+    
+        const isFitting = ( top, left, radius ) => top >= 0 && left >= 0 && sheetDimension.width > left + 2 * radius && sheetDimension.height > top + 2 * radius;
+    
+        const positions = [];
+        const { padding = 50 } = options;
+        const radius = (Math.max(buttonDimension.width, buttonDimension.height) / 2)+ padding;
+        
+        let top = padding;
+    
+        let currentRow = 0;
+    
+        while (top < sheetDimension.height) {
+            let left = currentRow % 2 == 0 ? padding : padding - radius;
+            while (left < sheetDimension.width) {
+                if ( isFitting(top, left, radius)) positions.push( [left, top] );
+                left = left + 2 * radius;
+            }
+            top = parseInt(top + (Math.sqrt(3) * radius));
+            currentRow++;
+        }
+    
+        return positions;
+    };
 
-    return positions;
+    const sheetCanvas = document.createElement("canvas");
+    sheetCanvas.width = width;
+    sheetCanvas.height = height;
+
+    const sheetCtx = sheetCanvas.getContext("2d");
+
+    buttonPositionsByWidth( { width: buttonCanvas.width, height: buttonCanvas.height }, { width, height } ).forEach( ([x,y ]) => sheetCtx.drawImage(buttonCanvas, x,y) )
+
+    return sheetCanvas;
 };
